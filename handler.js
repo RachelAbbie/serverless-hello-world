45103f79c05556f16e92b8f8aa3ddbcb6ddf5cec
CSCMWS\rfernando8 <rachelabbie.fernando@dxc.com>
2021-02-16 18:12:06 +0800

    final

--- handler.js
+++ handler.js
@@ -2,12 +2,16 @@
 
 module.exports.hello = async (event) => {
 
-  
+  let message = "HELLO WORLD";
+  const name = event.pathParameters && event.pathParameters.name;
+  if (name !== null){
+    message = `Hello ${name}`
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "HELLO WORLD",
+        message: message,
         input: event,
       },
       null,
